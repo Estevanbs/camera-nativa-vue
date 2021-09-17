9a47908b5ad9262afca713bf179e9394a8753e7b
Estevan Bartmann Silveira <estevam@inovaclick.com.br>
2021-09-17 09:26:02 -0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input
+        id="cameraFileInput"
+        type="file"
+        accept="image/*"
+        capture="environment"
+        @change="previewFiles"
+      />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+  previewFiles(event) {
+    console.log(event.target.files);
+  }
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
